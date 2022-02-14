20ed3fd983ae2fbbe373b23493d25e3774efc4b5
dafm10 <dafm.10@gmail.com>
2022-02-14 11:35:14 -0500

    Category List 2

--- lib/pages/admin/category_list_page.dart
+++ lib/pages/admin/category_list_page.dart
@@ -1,11 +1,24 @@
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_codigo4_menuapp/ui/general/colors.dart';
+import 'package:flutter_codigo4_menuapp/ui/widgets/general_widget.dart';
 
 class CategoryListPage extends StatelessWidget {
+  CollectionReference collectionReference =
+      FirebaseFirestore.instance.collection('categories');
 
   @override
   Widget build(BuildContext context) {
+    /*collectionReference.get().then((value){
+      value.docs.forEach((element) {
+        print(element.data());
+      });
+    });*/
+
+    //print(collectionReference.snapshots());
+
+    //print(collectionReference.snapshots().map((event) => event.docs));
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -24,10 +37,70 @@ class CategoryListPage extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0,),
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+            vertical: 12.0,
+          ),
           child: Column(
             children: [
-
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage("assets/images/logo.jpeg"),
+                    radius: 25.0,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Categorias",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                        Text(
+                          "Gestiona las categorías existentes",
+                          style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            color: COLOR_PRIMARY.withOpacity(0.6),
+                            fontSize: 14.0,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10.0,
+                        ),
+                        lineWidget,
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              StreamBuilder(
+                stream: collectionReference.snapshots(),
+                builder: (BuildContext context, AsyncSnapshot snap) {
+                  if (snap.hasData) {
+                    return Text("Hola");
+                  }
+                  return Center(
+                    child: SizedBox(
+                      height: 20.0,
+                      width: 20.0,
+                      child: CircularProgressIndicator(
+                        color: COLOR_BRAND_SECONDARY,
+                        strokeWidth: 2,
+                      ),
+                    ),
+                  );
+                },
+              ),
             ],
           ),
         ),
