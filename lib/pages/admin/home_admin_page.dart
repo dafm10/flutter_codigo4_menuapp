b01fb3ff4ebcd7f6ee1e8f695f4954d05f2e2e27
dafm10 <dafm.10@gmail.com>
2022-02-14 10:34:38 -0500

    Home Admin 2

--- lib/pages/admin/home_admin_page.dart
+++ lib/pages/admin/home_admin_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo4_menuapp/ui/general/colors.dart';
+import 'package:flutter_codigo4_menuapp/ui/widgets/general_widget.dart';
 
 class HomeAdminPage extends StatefulWidget {
   const HomeAdminPage({Key? key}) : super(key: key);
@@ -11,11 +13,105 @@ class _HomeAdminPageState extends State<HomeAdminPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        /*leading: const SizedBox(
+          width: 20.0,
+        ),*/
+        title: const Text(
+          "MenuApp",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: COLOR_PRIMARY,
+          ),
+        ),
+        centerTitle: true,
+      ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
           child: Column(
-            children: [],
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage("assets/images/logo.jpeg"),
+                    radius: 25.0,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Bienvenido Admin",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                          ),
+                        ),
+                        Text(
+                          "Administra todo desde este módulo",
+                          style: TextStyle(
+                            fontWeight: FontWeight.normal,
+                            color: COLOR_PRIMARY.withOpacity(0.6),
+                            fontSize: 14.0,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10.0,
+                        ),
+                        lineWidget,
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Container(
+                height: 100.0,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12.0),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(4, 4),
+                      blurRadius: 12.0,
+                      color: Colors.black87.withOpacity(0.07),
+                    ),
+                  ],
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzqiPAQ6N4heNSfYwwv1b8ZxqiMVQQ5Dm-ToA4jZcb4WiTDeq_lcG8C_VfYFNCp0WLiv0&usqp=CAU",
+                    ),
+                  ),
+                ),
+                child: Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Text(
+                          "Productos",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
