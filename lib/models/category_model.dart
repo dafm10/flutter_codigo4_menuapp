ba1d3c294ac19cbc3f89b2f4683b8778e319107e
dafm10 <dafm.10@gmail.com>
2022-02-13 21:34:18 -0500

    category model 1

---
+++ lib/models/category_model.dart
@@ -0,0 +1,25 @@
+
+class Category {
+
+  String id;
+  String description;
+  bool status;
+
+  Category({
+    required this.id,
+    required this.description,
+    required this.status,
+  });
+
+  factory Category.fromJson(Map<String, dynamic> json) => Category(
+    id: json["id"] ?? "",
+    description: json["description"],
+    status: json["status"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "description": description,
+    "status": status,
+  };
+}
