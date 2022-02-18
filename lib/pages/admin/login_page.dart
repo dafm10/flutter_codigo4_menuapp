896c5c0b5f61f67edfd8a8798ee240e498d66f55
dafm10 <dafm.10@gmail.com>
2022-02-17 19:22:26 -0500

    SingIn 2

--- lib/pages/admin/login_page.dart
+++ lib/pages/admin/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo4_menuapp/pages/admin/home_admin_page.dart';
+import 'package:flutter_codigo4_menuapp/ui/general/colors.dart';
 
 class LoginAdminPage extends StatefulWidget {
   const LoginAdminPage({Key? key}) : super(key: key);
@@ -9,29 +11,223 @@ class LoginAdminPage extends StatefulWidget {
 }
 
 class _LoginAdminPageState extends State<LoginAdminPage> {
-  _loginEmailPassword()  async{
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool isInvisible = true;
+
+  _loginEmailPassword() async {
     //print(FirebaseAuth.instance.currentUser);
-    UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+    /*UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
       email: "dafm.10@gmail.com",
       password: "9667921d",
-    );
+    );*/
     //print(await userCredential.user!.getIdToken());
-    print(userCredential.user);
+    //print(userCredential.user);
+
+    try {
+      UserCredential userCredential =
+          await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomeAdminPage()));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == "user-not-found") {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            backgroundColor: Colors.redAccent,
+            content: Text(
+              "El usuario no existe",
+            ),
+          ),
+        );
+      } else if (e.code == "wrong-password") {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            backgroundColor: Colors.redAccent,
+            content: Text(
+              "Contraseña incorrecta",
+            ),
+          ),
+        );
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Example"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            _loginEmailPassword();
-          },
-          child: Text(
-            "Login",
+      backgroundColor: Color(0xff0D0F0E),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 20.0,
+              ),
+              Image.asset(
+                "assets/images/logo.jpeg",
+                height: 240.0,
+                fit: BoxFit.cover,
+              ),
+              const Text(
+                "Inicio de Sesión",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              const Text(
+                "Administrador",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.normal
+                ),
+              ),
+              const SizedBox(
+                height: 30.0,
+              ),
+              Row(
+                children: const [
+                  Text(
+                    "Correo Electrónico",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              TextFormField(
+                controller: _emailController,
+                style: const TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintText: "Correo electrónico",
+                  hintStyle: const TextStyle(
+                    color: Colors.white54,
+                  ),
+                  filled: true,
+                  fillColor: Color(0xff2A2A2A),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                children: const [
+                  Text(
+                    "Contraseña",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              TextFormField(
+                controller: _passwordController,
+                obscureText: isInvisible,
+                style: const TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintText: "Contraseña",
+                  hintStyle: const TextStyle(
+                    color: Colors.white54,
+                  ),
+                  filled: true,
+                  fillColor: Color(0xff2A2A2A),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  errorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  focusedErrorBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      isInvisible = !isInvisible;
+                      setState(() {
+
+                      });
+                    },
+                    icon: const Icon(
+                      Icons.remove_red_eye,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                width: double.infinity,
+                height: 60.0,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Color(0xFFFC7345),
+                      COLOR_SECONDARY,
+                    ],
+                  ),
+                ),
+                child: ElevatedButton(
+                  onPressed: () {
+                    _loginEmailPassword();
+                  },
+                  child: const Text(
+                    "Iniciar Sesiñon",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.transparent,
+                      shadowColor: Colors.transparent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      )),
+                ),
+              ),
+            ],
           ),
         ),
       ),
